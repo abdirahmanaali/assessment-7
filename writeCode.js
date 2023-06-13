d1c774fc31558039f699313952769038d82b357b
Draman13 <abdirahmanabdiaziz@gmail.com>
2023-06-12 21:11:21 -0500

    finished writeCode.js

--- writeCode.js
+++ writeCode.js
@@ -1,5 +1,6 @@
 // 1) Sum Zero
 // Write a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
+console.log("part 1");
 
 // For example:
 function addToZero(nums) {
@@ -30,38 +31,63 @@ addToZero([1, 2, 3, -2]);
 
 // 2) Unique Characters
 // Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
+console.log("part 2");
 
 // For example:
 function hasUniqueChars(word) {
   for (let i = 0; i < word.length; i++) {
-    let unique = [];
-    word[i].indexOf()
+    for (let w = i + 1; w < word.length; w++) {
+      if (word[i] === word[w]) {
+        return console.log(false);
+      }
+    }
   }
+  return console.log(true);
 }
 
-// hasUniqueChars("Monday");
+hasUniqueChars("Monday");
 // -> True
 
-// hasUniqueChars("Moonday");
+hasUniqueChars("Moonday");
 // -> False
 
 // 3) Pangram Sentence
 // A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
-
+console.log("part 3");
 // Write a function to check a sentence to see if it is a pangram or not.
+function isPangram(word) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const lowercaseWord = word.toLowerCase();
 
-// For example:
+  for (let i = 0; i < alphabet.length; i++) {
+    const letter = alphabet[i];
+    if (!lowercaseWord.includes(letter)) {
+      return console.log(false);
+    }
+  }
 
-// isPangram("The quick brown fox jumps over the lazy dog!");
+  return console.log(true);
+}
+
+isPangram("The quick brown fox jumps over the lazy dog!");
 // -> True
 
-// isPangram("I like cats, but not mice");
+isPangram("I like cats, but not mice");
 // -> False
 
 // 4) Longest Word
 // Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
 
+console.log("part 4");
 // For example:
-
-// findLongestWord(["hi", "hello"]);
+function findLongestWord(words) {
+  for (let word = 0; word < words.length; word++) {
+    for (let i = word; i < words.length; i++) {
+      if (words[word] < words[i]) {
+        return words[word];
+      }
+    }
+  }
+}
+console.log(findLongestWord(["hi", "hello", "Pokemon Ruby"]));
 // -> 5
